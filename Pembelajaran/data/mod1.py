c1874d6826cd1652ba4552ef9cb519571a0044b8
Vista Sasmita Padmanagara <73339446+vistasasmitapadmanagara@users.noreply.github.com>
2022-05-13 16:02:42 +0700

    Add files via upload

---
+++ Pembelajaran/data/mod1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def foo():
+    print('[mod1] foo()')
+    
+class Foo:
+    pass
+
+
+# In[ ]:
+
+
+
+
---
+++ Pembelajaran/data/mod2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def bar():
+    print('[mod2] bar()')
+    
+class Bar:
+    pass
+
+
+# In[ ]:
+
+
+
+
