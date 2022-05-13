c2b58c2c8c05e9348de69868a9cbc80d6c42d4a6
Vista Sasmita Padmanagara <73339446+vistasasmitapadmanagara@users.noreply.github.com>
2022-05-13 14:13:19 +0700

    Add files via upload

---
+++ Pembelajaran/data/mod.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+s = "Hello, my name is Heylen."
+a = [10,20,30]
+
+def foo(arg):
+    print (f'arg={arg}')
+           
+class Foo:
+    pass
+
